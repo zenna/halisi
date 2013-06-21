a120d4b5c1d9cdd09244e49c0d4f5ac13ba405c3
Zenna Tavares <zennatavares@gmail.com>
2013-06-21 09:46:11 -0400

    added tests and git ignore

---
+++ test/relax/graphics_test.clj
@@ -0,0 +1,14 @@
+(ns relax.graphics-test
+  (:use clojure.test
+        relax.graphics
+        clozen.helpers))
+
+(def conv-poly
+  [[0 0] [5 0] [5 5] [2.5 5] [0 5]])
+
+(deftest convex?-test
+  (let [conv-poly [[0 0] [5 0] [5 5] [2.5 5] [0 5]]
+        non-conv-poly [[0 0] [5 0] [5 5] [2.5 4.5] [0 5]]
+        complex [[1 3] [9 7] [7 9] [7 2] [9 6] [1 8]]]
+    (is (= (convex? conv-poly) true))
+    (is (= (convex? non-conv-poly) false))))
