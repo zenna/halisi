89452e6ecf9408d9a6bc6226ef85a7155a3824a7
Zenna Tavares <zennatavares@gmail.com>
2013-08-19 15:28:50 -0400

    first expansion working

--- src/relax/box.clj
+++ src/relax/box.clj
@@ -2,7 +2,7 @@
                 :author "Zenna Tavares"}
   relax.box
   (:use relax.abstraction)
-  ;(:use clozen.helpers)
+  (:use clozen.helpers)
   (:require [clojure.math.combinatorics :as combo]))
 
 (defn lower-bound [interval]
@@ -71,6 +71,14 @@
   [box1 box2]
   "Do they intersect?"
   (not (not-intersect? box1 box2)))
+
+(defn point-in-abstraction?
+  "Is a point within an abstraction"
+  [box point]
+  {:pre [(count= (:internals box) point)]}
+  ; (println "PIA" point box (intersect? box (make-abstraction (mapv #(vector % %) point) 'no-formula)))
+  ; Assume point is degenerate box
+  (intersect? box (make-abstraction (mapv #(vector % %) point) 'no-formula)))
 
 (defn abstraction-contains?
   "Does box-a fully contain box-b?
@@ -142,28 +150,118 @@
        (rand (- (upper-bound interval) (lower-bound interval))))))
 
 ;; Boolean Operations
+(defn remove-dim
+  "Remove a dimension from a box"
+  [box dim]
+  (assoc box :internals (vec-remove (:internals box) dim)))
 
+; TODO
+(defn edit-interval
+  ""
+  [box dim interval]
+  (assoc-in box [:internals dim] interval))
 
+; TODO
+(defn edit-lower-bound
+  [box dim new-bound]
+  (assoc-in box [:internals dim 0] new-bound))
 
-; (defn expand-box
-;   [box other-boxes]
-;   ;1. Choose a dimension.
-;   ;2. Sort the boxes according to that dimension
-;   ; (memoize this so it only needs to be done d times)
-;   ;3. Starting at self, and increasing with boxes in correct dimension
-;   ; ask if I were to extend current box here would it be satisfiable
-;   ; Would it collide any other boxes
-;   ; How to check satisfiable
-;   ; Checking collisions is an intersect check for all other boxes, linear in worst case 
-;   ; And could do better using a spatial data structure if necessary
-;   ; 
+(defn edit-upper-bound
+  [box dim new-bound]
+  (assoc-in box [:internals dim 1] new-bound))
 
+; FIXME, IM SAMPLING WITHIN THE ENTIRE EXPANDED BOX NOT JUST THE EXPANDED
+;REGION
+(defn valid-ext
+  "LOLZ!"
+  [box exp-box boxes]
+  (println "ext" exp-box)
+  (let [n-samples 100]
+    (loop [n-samples n-samples]
+      (println n-samples)
+      (cond
+        (zero? n-samples)
+        true
+
+        :else
+        (let [sample (abstraction-sample exp-box)]
+          (if (some #(point-in-abstraction? % sample) boxes)
+              (recur (dec n-samples))
+              false))))))
+
+(defn expand-box
+  [box boxes]
+  (let [n-dims (num-dims box)]
+  (loop [exp-box box sides (vec (range n-dims))]
+    (cond
+      (empty? sides)
+      exp-box
+      
+      :else
+      (let [[dim sides] (rand-vec-remove sides)
+            interval (nth-dim-interval box dim)
+            ; We only care about extending to places which intersect.
+            good-boxes (filter #(intersect? (remove-dim exp-box dim)
+                                            (remove-dim % dim))
+                                boxes)
+            pvar (println "good boxes" good-boxes)
+
+            ext-points (sort (flatten (map #(nth-dim-interval % dim) 
+                                            good-boxes)))
+
+            pvar (println "ext points" ext-points)
+            pvar (println "filtered" (filterv #(>= % (upper-bound interval)) ext-points))
+            upper
+            (max-pred #(valid-ext exp-box
+                                  (edit-upper-bound exp-box dim %)
+                                  boxes)
+                            (filterv #(>= % (upper-bound interval)) ext-points))
+            pvar (println "upper" upper)
+
+            lower
+            (min-pred #(valid-ext exp-box
+                                  (edit-lower-bound exp-box dim %)
+                                  boxes)
+                            (filterv #(<= % (lower-bound interval)) ext-points))]
+        (recur (edit-interval exp-box dim [lower upper]) sides))))))
+
+; (defn select-init-box [boxes]
+;   (first boxes))
+
+; (defn can-extend?
+;   [side])
+
+; ; NO TEST
 ; (defn cover-abstr
 ;   "Given"
-;   [& boxes]
-;   1. Get the initial box1.
-;   Now what I need to expand the box.
-;   ;Expand box to 
+;   [boxes]
+;   (loop [unvisited-sides (select-init-box boxes) covering [curr-box]]
+;     (if (empty? unvisited-sides)
+;         covering
+;         (let [side (random-nth unvisited-sides)]
+;           (if (can-extend? side)
+;               (let [new-box (expand-box (extend-box side))]
+;                 (recur (add-sides unvisited-sides new-box) (conj covering new-box)))
+;               (recur (remove unvisited-sides sides) covering))))))
+
+; ; I could check each face of each box every tiem but that's silly
+; ; I could carry around a list of all the things I've alredy done
+; ; Or I could carry around a list of ones available to carry from
+
+; (defn cover-abstr-inner
+;   [init boxes])
+
+  ; Assume overlapping or divide into overlapping segments?
+  ; Find feasible region?
+  ; Start with any box
+  ; - Start with overlapping region
+  ; - Start with point sampled uniformly from volume
+  ; Breadth first, so I'm going to try to expand the dimension of each box
+  ; Choose a side uniformly and expand each dimension one
+  ; Q- What is the set of lines to which I must consider expansion
+  ; When that is done choose a side randomly, and see if extension is possible, if so extend
+  ; 
+  ;Expand box to
 
 
 ; (defn tile-abstr
@@ -192,4 +290,5 @@
          [[(rand) (rand)][(rand) (rand)]]))))
 
 (defn -main []
-  (union-volume b1 b2 b3))
+  ; (union-volume b1 b2 b3))
+  (expand-box (overlap b1 b2) [b1 b2]))
