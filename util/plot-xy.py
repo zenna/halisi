af6b43aa0a52da37e0bdde9dc944b74b3697450d
Zenna Tavares <zennatavares@gmail.com>
2013-11-21 08:18:03 -0500

    plotting works

--- util/plot-xy.py
+++ util/plot-xy.py
@@ -1,9 +1,24 @@
 #!/usr/bin/env python
 from sys import argv
-from pylab import *
+import matplotlib.pyplot as plt
 import numpy as np
+from StringIO import StringIO
 
-X = np.loadtxt(argv[1])
-plot(X[0],X[1])
+legends = []
 
-show()
+bucket_file = open(argv[1])
+for line in bucket_file:
+  line = line.strip().split(",")
+
+  x_vals_str = line[2].strip("[").strip("]")
+  y_vals_str = line[3].strip("[").strip("]")
+  x_vals = np.loadtxt(StringIO(x_vals_str))
+  y_vals = np.loadtxt(StringIO(y_vals_str))
+
+  legends.append(line[0] + line[1])
+  plt.plot(x_vals, y_vals)
+  print "x", x_vals, "y", y_vals
+
+plt.legend(legends , loc = 'upper right')
+plt.title('Bucket Scaling Plot')
+plt.show()
