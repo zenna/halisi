dacf30ab32310a099c4a0d359435a18bc5685c9d
Zenna Tavares <zennatavares@gmail.com>
2014-03-03 19:51:43 -0500

    moved pattern matching to construct

--- src/relax/construct.clj
+++ src/relax/construct.clj
@@ -62,6 +62,95 @@
       (recur new-exp (next-agent exp new-exp))
       exp)))
 
+(def primitives {'+ + '* * '- +})
+
+(defn primitive
+  "Get evaluable function from symbol"
+  [symb]
+  (if-let [f (primitives symb)]
+    f
+    (eval symb)))
+
+(defn primitive?
+  "Is the symbol a primitive?"
+  [symb]
+  (clzn/nil-to-false (primitives symb)))
+
+(def fn-codes
+  {repeat
+  '(fn [n f]
+     (loop [n n res []]
+      (if (zero? n) res
+          (recur (dec n) (conj res (f))))))})
+
+(defn lookup-compound
+  [f]
+  (fn-codes f))
+
+(defn compound?
+  [f]
+  (if (lookup-compound f)
+      true
+      false))
+
+(comment
+  (def a-rule
+    (rule '->
+          `(~'square ~(variable 'x)) ; lhs
+          (fn [{x (variable 'x)}]           ; rhs
+            `(~'* ~x ~x))
+          (fn [_]           ; condition
+            true)))
+
+  (def mul-rule
+    (rule '->
+          `(~'* ~(variable 'x) ~(variable 'y)) ; lhs
+          (fn [{x (variable 'x) y (variable 'y)}]           ; rhs
+            (* x y))
+          (fn [{x (variable 'x) y (variable 'y)}]           ; condition
+            (and (number? x) (number? y)))))
+
+  ;; For primitive function evaluation I need to know that the arguments
+  ;; are fully evaluated.
+  ;; We can put that condition in the rule itself or not
+  ;; It seems like we'll be violating a separation principle if we put in the rule
+  ;; In the metacircular evaluator, we use order to avoid this problem.
+  ;; We could imagine a 
+  (def primitive-apply-rule
+    "This rule applies a primitive function"
+    (rule '->
+          `(~(variable 'f) ~(variable 'x) ~(variable 'y))
+          (fn [{f (variable 'f) x (variable 'x) y (variable 'y)}]
+            ((primitive f) x y))
+          (fn [{f (variable 'f) x (variable 'x) y (variable 'y)}]
+            (primitive? f))))
+
+  (def compound-f-sub-rule
+    "Substitute in a compound function"
+    (rule '->
+          (~(variable 'f) ~(variable 'x) ~(variable 'y))
+          (fn [{f (variable 'f) x (variable 'x) y (variable 'y)}]
+            `(~(lookup-compound f) x y))
+          (fn [{f (variable 'f) x (variable 'x) y (variable 'y)}]
+            (compound? f))))
+
+  (def variable-substitution
+    "Substitute in variables"
+    (rule '->
+          ()
+
+  ;; Options we can say, once we find pattern, replace all children using postwalk
+  ;; Or something
+  ;; Or (rewrite body eager-transfomer [(rule -> xs ys)])
+  ;; Or go from the bottom up, whenver I see a symbol, check what it's bound to
+  (-> ((fn [?xs] ?body) ?ys) 
+
+  (def a-exp '(+ (square (square 3)) 2))
+  (def b-exp '(* 2 9))
+  (def transformer (partial eager-transformer [a-rule primitive-apply-rule]))
+  (rewrite a-exp transformer)
+  (def result (rewrite a-exp transformer)))
+
 (comment
   (def a-pattern-agent (->PatternAgent [primitive-f self-eval]))
   (def exp '(+ 1 2 (* 3 0)))
