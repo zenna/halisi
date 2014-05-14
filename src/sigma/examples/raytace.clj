448bedbe819047a532309f86bd58f87c2d1b0cf0
Zenna Tavares <zennatavares@gmail.com>
2014-05-14 01:18:34 -0400

    raytrace test works

--- src/sigma/examples/raytace.clj
+++ src/sigma/examples/raytace.clj
@@ -1,8 +1,36 @@
 (ns sigma.examples.raytrace
-  (:require [clozen.helpers :refer :all]))
+  (:require [clozen.helpers :refer [tolerant=]]))
 
-(defn parallel? [v q]
-  (apply = (map / v q)))
+;; What's wrong with this
+;; Inconsistent naming for vectors, some times use v1 v2, v w, p q
+;; Pre conditions arent compile time
+;; Some functions take two points as input others take two vectors, not clear
+;; Inconsistent naming for 2 dimension dot2 , perp-2d
+;; Destructuring not supported by sigma
+;; Avoidable general recursion
+;; Shorthand for anon function unsupported by sigma
+
+(defn tolerant=
+  [x y]
+  (< (* (- x y) (- x y)) 0.00001))
+
+(defn num-dims [v] (count v))
+
+(defn parallel?
+  "parallel if w1vi-wiv1=0 for all i."
+  [v w]
+  {:pre [(= (num-dims v) (num-dims w))
+         (> (num-dims v) 1)]}
+  (let [w1 (first w)
+        v1 (first v)]
+    (every? #(tolerant= 0.0 %) (map (fn [vi wi] (- (* w1 vi) (* wi v1)))
+                                    (rest v) (rest w)))))
+
+(defn perp-2d
+  "2D perp operator"
+  [[x y :as vect]]
+  {:pre [(= 2 (count vect))]}
+  [(- y) x])
 
 (defn points-to-vec
   "convert pair of points to vector"
@@ -15,7 +43,7 @@
    returns t parameter on line [a0 a1].
    fraction of distane from a0 to a1."
   [[a0 a1 :as v] [b0 b1 :as q]]
-  {:pre [(not (parallel? v q))]}
+  {:pre [(not (parallel? (points-to-vec a0 a1) (points-to-vec b0 b1)))]}
   (let [[u1 u2] (points-to-vec a0 a1)
         [v1 v2] (points-to-vec b0 b1)
         [w1 w2] (points-to-vec b0 a0)
@@ -58,24 +86,36 @@
 (defn reflects?
   "Find closest intersection point from p0 to an obstacle, ensure  between these points"
   [obstacles p1 p2 p3]
-  (let [closest-o (min-index (map #(intersection-point [p1 p2] %) obstacles))]
-    (= (dot2 (points-to-vec p1 p2) (nth obstacles closest-o))
-       (dot2 (nth obstacles closest-o) (points-to-vec p1 p2)))))
+  (let [intersection-points (mapv #(first (intersection-point [p1 p2] %)) obstacles)
+        closest-o (second (min-index intersection-points))
+        obstacle-vec (apply points-to-vec (nth obstacles closest-o))]
+    (dot2 obstacle-vec (points-to-vec p2 p3))
+    (tolerant= (dot2 (points-to-vec p1 p2) obstacle-vec)
+               (- (dot2 obstacle-vec (points-to-vec p2 p3))))))
 
 (defn path-is-ray?
-  "Path is an array with respect to set of obstacles if"
-  [obstacles path]
+  "Path is a ray with respect to set of obstacles if"
+  [path]
   (every-conseq-n? 3 (partial reflects? obstacles) path))
 
-(def obstacle [[0.0 5.0] [10.0 5.0]])
-(def obstacles [obstacle])
-(def path [[0.0 0.0] [5.0 5.0] [10.0 0.0]])
-(path-is-ray? obstacles path)
+;; Sanity tests
+(comment
+  (def obstacle [[0.0 5.0] [10.0 5.0]])
+  (def obstacles [obstacle])
+  (def path [[0.0 0.0] [5.0 5.0] [10.0 0.0]])
+  (path-is-ray? obstacles path))
 
 ;; Queries
 (comment
   (def ray (condition (uniform 0 10 0 10 5) (partial path-is-ray? obstacles)))
-  (def a-ray (sample ray))
+  (def path-outside-rect? [path rect]
+
   (def ray-outside-container (probability ray outside-container?)))
 
-;; Ray-tracing
+;; Ray-tracing example 2 - dadada============================================
+(defn [x-min x-max y-min y-max n-points obstacles]
+  "Uniformly sample start point"
+  (let [p0 [(uniform x-min x-max) (uniform y-min uniform y-max)]
+        p0.5 [(uniform x-min x-max) (uniform y-min uniform y-max)]
+        ])
+    ()
