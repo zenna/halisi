36c21f5e7cdba5bed9c228e267494727d532e910
Zenna Tavares <zennatavares@gmail.com>
2014-07-01 05:32:34 +0100

    removed lp

--- project.clj
+++ project.clj
@@ -2,38 +2,16 @@
 (require 'leiningen.core.eval)
 ;;(println (leiningen.core.eval/get-os)) ;; try lein deps to see this
 
-(def LWJGL-CLASSIFIER
-  "Per os native code classifier"
-  {:macosx "natives-osx"
-   :linux "natives-linux"
-   :windows "natives-windows"}) ;; TESTME
-
-(defn lwjgl-classifier
-  "Return the os-dependent lwjgl native-code classifier"
-  []
-  (let [os (leiningen.core.eval/get-os)]
-    (get LWJGL-CLASSIFIER os)))
-
 (defproject sigma "0.1.0-SNAPSHOT"
   :description "Self sigmaing programs"
   :url "https://github.com/rogerallen/hello_lwjgl"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :plugins [[lein-marginalia "0.7.1"]
-            [com.keminglabs/cljx "0.3.2"]
+  :plugins [[com.keminglabs/cljx "0.3.2"]
             [lein-cljsbuild "1.0.2"]]
   :dependencies [[org.clojure/clojurescript "0.0-2030"]
                  [org.clojure/clojure "1.6.0"]
-                 [lpsolve "5.5.2.0"]
-                 [incanter "1.4.1"]
-                 [criterium "0.4.1"]
-                 [org.lwjgl.lwjgl/lwjgl "2.8.5"]
-                 [org.lwjgl.lwjgl/lwjgl_util "2.8.5"]
                  [org.clojure/math.combinatorics "0.0.4"]
-                 [org.clojure/data.priority-map "0.0.4"]
-                 [org.lwjgl.lwjgl/lwjgl-platform "2.8.5"
-                  :classifier ~(lwjgl-classifier)
-                  ]
                  [org.clojure/tools.trace "0.7.5"]
                  [fipp "0.4.1"]
                  [org.clojure/core.match "0.2.1"]
@@ -56,6 +34,4 @@
                                 :optimizations :whitespace
                                 :pretty-print true}}}}
 ;;   :hooks [cljx.hooks]
-  :main sigma.core
-  :jvm-opts [~(str "-Djava.library.path=/usr/local/lib:native/:"
-                     (System/getProperty "java.library.path"))])
+  :main sigma.core)
